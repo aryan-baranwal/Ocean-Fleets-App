ff7f5592a549d6f56f6fd9d20c5b24c6670f07bf
Aryan Baranwal <aryanbaranwal2004@gmail.com>
2026-02-09 17:43:29 +0530

    [Aryan Baranwal] Added UC5: add console based user interaction

---
+++ src/ui/UserInterface.java
@@ -0,0 +1,57 @@
+package ui;
+
+import java.util.List;
+import java.util.Scanner;
+import model.Vessel;
+import util.VesselUtil;
+
+public class UserInterface {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        VesselUtil vesselUtil = new VesselUtil();
+
+        System.out.println("Enter the number of vessels to be added");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter vessel details");
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            String[] parts = input.split(":");
+
+            Vessel vessel = new Vessel(
+                    parts[0],
+                    parts[1],
+                    Double.parseDouble(parts[2]),
+                    parts[3]
+            );
+
+            vesselUtil.addVesselPerformance(vessel);
+        }
+
+        System.out.println("Enter the Vessel Id to check speed");
+        String searchId = scanner.nextLine();
+
+        Vessel found = vesselUtil.getVesselById(searchId);
+        if (found != null) {
+            System.out.println(found.getVesselId() + " | " +
+                    found.getVesselName() + " | " +
+                    found.getVesselType() + " | " +
+                    found.getAverageSpeed() + " knots");
+        } else {
+            System.out.println("Vessel Id " + searchId + " not found");
+        }
+
+        System.out.println("High performance vessels are");
+        List<Vessel> highPerf = vesselUtil.getHighPerformanceVessels();
+        for (Vessel vessel : highPerf) {
+            System.out.println(vessel.getVesselId() + " | " +
+                    vessel.getVesselName() + " | " +
+                    vessel.getVesselType() + " | " +
+                    vessel.getAverageSpeed() + " knots");
+        }
+
+        scanner.close();
+    }
+}
