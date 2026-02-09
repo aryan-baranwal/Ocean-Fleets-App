674f1a1198c06296da672b8986f3391787344024
Aryan Baranwal <aryanbaranwal2004@gmail.com>
2026-02-09 17:33:03 +0530

    [Aryan Baranwal] Added UC3: retrieve vessel by vesselId

--- src/util/VesselUtil.java
+++ src/util/VesselUtil.java
@@ -15,4 +15,12 @@ public class VesselUtil {
     public void addVesselPerformance(Vessel vessel) {
         vesselList.add(vessel);
     }
+    public Vessel getVesselById(String vesselId) {
+        for (Vessel vessel : vesselList) {
+            if (vessel.getVesselId().equals(vesselId)) {
+                return vessel;
+            }
+        }
+        return null;
+    }
 }
